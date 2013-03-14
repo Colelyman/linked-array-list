6285657c9a0f5a55b300149cae23dcb95fbb2229
Cole Lyman <cole@colelyman.com>
2013-03-14 17:58:22 -0600

    Add function declarations to list class

---
+++ list.cpp
@@ -0,0 +1,24 @@
+#include "list.h"
+
+list::list(int capacity) {
+	this->capacity = capacity;
+	tail = NULL;
+	head = NULL;
+	list_size = 0;
+}
+
+void list::insert(int index, const type& item) {
+
+}
+
+Node* list::remove(int index) {
+
+}
+
+Node* list::find(type item) {
+
+}
+
+void list::print() {
+
+}
