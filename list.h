4ba25b99289ae09b6ebbf0778b82696b3bb82a8a
Cole Lyman <cole@colelyman.com>
2013-03-14 17:46:24 -0600

    Add function and data definitions to list class

--- list.h
+++ list.h
@@ -3,6 +3,20 @@
 template<typename type>
 class list {
 public:
+	list(int capacity);
+	void insert(int index, const type& item);
+	Node* remove(int index);
+	Node* find(type item);
+	void print();
 private:
+	int capacity;
+	struct Node {
+		Node* prev;
+		Node* next;
+		type items[capacity];
+		int size;
+	};
+	Node* head, tail;
+	int list_size;
 };
 #endif
