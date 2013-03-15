409467943cc5e8f7f8e6c22f2be1a79e10162cb5
Cole Lyman <cole@colelyman.com>
2013-03-15 12:45:34 -0600

    Implement insert()

--- list.h
+++ list.h
@@ -13,6 +13,7 @@ private:
 		Node() {
 			prev = NULL;
 			next = NULL;
+			size = 0;
 		}
 	};
 	int capacity;
@@ -27,8 +28,30 @@ public:
 		list_size = 0;
 	}
 
-	void insert(int index, const ItemType& item) {
+	void split(Node* n) {
+		Node* n2 = new Node();
+		for(int i = capacity / 2, j = 0; i < capacity; i++, j++)
+			n2->items[j] = n->items[i];
+	}
 
+	void insert(int index, const ItemType& item) {
+		Node* n = find(index);
+		if(n == NULL) {
+			n = new Node();
+			n->items[n->size] = item;
+			n->size++;
+		}
+		else if(n->size < capacity) {
+			n->items[n->size] = item;
+			n->size++;
+		}
+		else {
+			split(n);
+			n = find(index);
+			n->items[n->size] = item;
+			n->size++;
+		}
+		list_size++;
 	}
 
 	Node* remove(int index) {
